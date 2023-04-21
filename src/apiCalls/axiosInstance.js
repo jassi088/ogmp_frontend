0bdec7e4d02b99c63a311b745c6713b72e56a561
Jaspreet Singh <sandhujassi088@gamil.com>
2023-04-22 03:01:53 +0530

    'production-ready'

--- src/apiCalls/axiosInstance.js
+++ src/apiCalls/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
+    baseURL: 'https://ogmp.onrender.com',
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`,
-    }, 
+    },
 });
